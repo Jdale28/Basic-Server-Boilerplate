fe3960f7673e60051807bb5e4eaba6f73e5037da
Jordan Dale <jordan.dale2@gmail.com>
2018-10-23 11:39:07 -0400

    Added mongoose and seeds 'boilerplate' examples

--- server.js
+++ server.js
@@ -54,3 +54,86 @@ const applicationController = {
 
 module.exports = applicationController
 // End of application.js "boilerplate" within controllers
+
+
+
+
+// In your console run 'npm i mongoose' (no quotes)
+// In your code, create a folder named db with a file named connections.js
+// The following is "boilerplate" for using Mongoose
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/store-samples')
+mongoose.connection.once('open', () = (
+    console.log('Mongoose has connected to MongoDb')
+))
+
+module.exports = mongoose
+// End of Mongoose "boilerplate"
+
+
+
+
+// Beginning of Model "boilerplate" with store and products as examples
+// Beginning of store.js model "boilerplate"
+// Create a folder called Models and a file named store.js
+const mongoose = require('../db/connections')
+const Schema = mongoose.schema
+const Store = new Schema ({
+    name: String,
+    location: String,
+    hours: String,
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ]
+})
+// End of store model "boilerplate"
+
+
+
+// Beginning of product.js model "boilerplate"
+// Create a file named product.js within model folder
+const mongoose = require('../db/connections')
+const Schema = mongoose.schema
+const Product = new Schema ({
+    name: String,
+    proce: Number,
+})
+// end of product.js "boilerplate"
+// end of Model folder "boilerplate"
+
+
+
+
+// Beginning of seeds "boilerplate"
+// In your code, create a folder called db and a folder named seeds.js 
+const Store = require('../models/Store.js')
+const Product = require('../models/Product.js')
+const mongoose = require('./connections')
+
+const apple = new Product ({
+    name: "MacBook Pro",
+    price: 3
+})
+
+const bread = new Product ({
+    name: "MacBook Pro",
+    price: 5
+})
+
+const kroger = new Store({
+    name: "Kroger",
+    location: "Edgewood Shopping Center",
+    hours: "Open 24hrs",
+    products: [apple, bread]
+})
+
+Store.remove({})
+    .then(() => Product.insertMany([apple, bread]))
+    .then(() => kroger.save())
+    .then(() => target.save())
+    .then(() => console.log("Database seeded successfully"))
+    .then(() => mongoose.connection.close())
+// End of seed.js "boilerplate"
